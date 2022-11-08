d5551cdf2ae8fdd0ab11515b98aa58ce20bcefd0
ClaireLiseMadjarian <claire.madjarian@mines-ales.org>
2022-11-08 11:04:40 +0100

    Coucou

--- Scrapping.py
+++ Scrapping.py
@@ -1,2 +1,5 @@
-print("hello world")
-print("Coucou, c'est Claire!")
+from bs4 import BeautifulSoup
+import requests
+
+with open("https://digitalcomicmuseum.com") as f:
+    soup = BeautifulSoup(f,'html.parser')
